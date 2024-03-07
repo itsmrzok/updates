e305ab2938950afda01f6acaea62124ce5189329
itsmrzok <abbaszameer234@gmail.com>
2024-03-07 17:10:30 +0500

    addition

--- src/com/example/AvlTree.java
+++ src/com/example/AvlTree.java
@@ -1,78 +1,68 @@
 package com.example;
+
 import java.nio.file.*;
 import java.io.*;
 import java.util.List;
+
 public class AvlTree {
     static Node root;
 
     public static Node searchData(Node roott, String name) {
-        if (roott == null || roott.product_name.equals(name))
+        if (roott == null || roott.product_name.equalsIgnoreCase(name)) // Use equalsIgnoreCase for case-insensitive comparison
             return roott;
-
-        else if (roott.product_name.compareToIgnoreCase(name) >0)
+        else if (roott.product_name.compareToIgnoreCase(name) > 0)
             return searchData(roott.left, name);
-
         else
             return searchData(roott.right, name);
     }
+
     public Node insert(Node new_node) {
-        if (root == null)
-        {
+        if (root == null) {
             root = new_node;
-        }
-        else
-        {
+        } else {
             Node temp = root;
-            while (temp != null)
-            {
-                if (temp.product_name.equals(new_node.product_name))
-                {
+            while (temp != null) {
+                if (temp.product_name.equals(new_node.product_name)) {
                     return temp;
-                }
-                else if (temp.product_name.compareToIgnoreCase(new_node.product_name) >0 && (temp.left == null))
-                {
+                } else if (temp.product_name.compareToIgnoreCase(new_node.product_name) > 0 && (temp.left == null)) {
                     temp.left = new_node;
                     break;
-                }
-                else if (temp.product_name.compareToIgnoreCase(new_node.product_name)>0)
-                {
+                } else if (temp.product_name.compareToIgnoreCase(new_node.product_name) > 0) {
                     temp = temp.left;
-                }
-                else if (temp.product_name.compareToIgnoreCase(new_node.product_name)<0 && (temp.right == null))
-                {
+                } else if (temp.product_name.compareToIgnoreCase(new_node.product_name) < 0 && (temp.right == null)) {
                     temp.right = new_node;
                     break;
-                }
-                else
-                {
+                } else {
                     temp = temp.right;
                 }
             }
         }
-        root=rebalance(root);
+        root = rebalance(root);
         return root;
     }
+
     Node rebalance(Node root) {
         updateHeight(root);
         int balance = getBalance(root);
         if (balance > 1) {
             Node temp = root.right;
-            if (height( temp.right) > height(temp.left)) {
-                root =  rotateLeft(root);
+            if (height(temp.right) > height(temp.left)) {
+                root = rotateLeft(root);
             } else {
-                root.right = rotateRight( root.right);
-                root =  rotateLeft(root);
+                root.right = rotateRight(root.right);
+                root = rotateLeft(root);
             }
         } else if (balance < -1) {
-            if (height(root.left.left) > height( root.left.right))
-                root =  rotateRight(root);
+            if (height(root.left.left) > height(root.left.right))
+                root = rotateRight(root);
             else {
-                root.left =  rotateLeft(root.left);
-                root =  rotateRight(root);
+                root.left = rotateLeft(root.left);
+                root = rotateRight(root);
             }
         }
         return root;
     }
+
     Node rotateLeft(Node root) {
         Node rr = (Node) root.right;
         Node temp = rr.left;
@@ -82,6 +72,7 @@ public class AvlTree {
         updateHeight(rr);
         return rr;
     }
+
     Node rotateRight(Node root) {
         Node rr = root.left;
         Node temp = rr.right;
@@ -91,19 +82,22 @@ public class AvlTree {
         updateHeight(rr);
         return rr;
     }
+
     void updateHeight(Node n) {
         n.height = 1 + Math.max(height(n.left), height((Node) n.right));
     }
+
     int height(Node n) {
         return n == null ? 0 : n.height;
     }
+
     int getBalance(Node n) {
         return (n == null) ? 0 : height((Node) n.right) - height(n.left);
     }
 
     static List<String> allLines = null;
-    public static List<String> readFileInList(String fileName)
-    {
+
+    public static List<String> readFileInList(String fileName) {
         try {
             allLines = Files.readAllLines(Paths.get(fileName));
         } catch (IOException e) {
@@ -111,16 +105,15 @@ public class AvlTree {
         }
         return allLines;
     }
-    public List ReadALLData(Node root,String pathname)
-    {
-        int price =30000;
+
+    public List ReadALLData(Node root, String pathname) {
+        int price = 30000;
         List<String> l = readFileInList(pathname);
         for (int i = 0; i < l.size(); i++) {
-
-            Node n = new Node(l.get(i),price );
-            price+=5000;
-            root= insert( n);
+            Node n = new Node(l.get(i), price);
+            price += 5000;
+            root = insert(n);
         }
-        return allLines ;
+        return allLines;
     }
 }
